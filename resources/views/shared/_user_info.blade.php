733842af8f8bf742446b8749c2e823dd49ab8fc5
wangsongqing <xmwme@qq.com>
2022-02-11 15:59:32 +0800

    用户微博列表

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('users.show', $user->id) }}">
     <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
 </a>
-<h3>用户名:{{ $user->name }}</h3>
-<h3>邮  箱:{{ $user->email }}</h3>
+<h5>用户名:{{ $user->name }}</h5>
+<h5>邮  箱:{{ $user->email }}</h5>
